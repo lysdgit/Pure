575cae972d1427afbed5c0456f142d9899a40ee7
lilog <44893351+LeeYonAs@users.noreply.github.com>
2022-08-24 17:51:40 +0800

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,42 @@
+﻿const searchEngines = [{
+    "id": 0,
+    "name": "百度",
+    "img": "url('search\/baidu.svg')",
+    "url": "https:\/\/www.baidu.com\/s?wd="
+},
+{
+    "id": 1,
+    "name": "哔哩哔哩",
+    "img": "url('logo\/bilibili.png')",
+    "url": "https://search.bilibili.com/all?keyword="
+},
+{
+    "id": 2,
+    "name": "知乎",
+    "img": "url('logo\/zhihu.png')",
+    "url": "https://www.zhihu.com/search?type=content&q="
+},
+{
+    "id": 3,
+    "name": "Google",
+    "img": "url('search\/google.svg')",
+    "url": "https:\/\/www.google.com\/search?q="
+},
+{
+    "id": 4,
+    "name": "Bing",
+    "img": "url('search\/bing.svg')",
+    "url": "https:\/\/cn.bing.com\/search?q="
+},
+{
+    "id": 5,
+    "name": "Github",
+    "img": "url('search\/github.svg')",
+    "url": "https:\/\/github.com\/search?q="
+},
+{
+    "id": 6,
+    "name": "谷歌镜像",
+    "img": "url('search\/google.svg')",
+    "url": "https://xn--flw351e.ml/search?q="
+}];
